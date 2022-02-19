a53a6043a9a9c88f235e2690a281d0f5e4a209ee
reggiepy <wangtongpy@foxmail.com>
2022-02-19 23:38:23 +0800

    (feature): init & Available log services

---
+++ log/server.go
@@ -0,0 +1,48 @@
+package log
+
+import (
+	"fmt"
+	"io/ioutil"
+	stlog "log"
+	"net/http"
+	"os"
+)
+
+var log *stlog.Logger
+
+type filelog string
+
+func (fl filelog) Write(data []byte) (int, error) {
+	f, err := os.OpenFile(string(fl), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	return f.Write(data)
+}
+
+func Run(destination string) {
+	log = stlog.New(filelog(destination), "go: ", stlog.LstdFlags)
+}
+
+func RegisterHandlers() {
+	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			msg, err := ioutil.ReadAll(r.Body)
+			if err != nil || len(msg) == 0 {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			write(string(msg))
+			_, _ = fmt.Fprintf(w, "success!\n")
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+	})
+}
+
+func write(msg string) {
+	log.Printf("%v\n", msg)
+}
---
+++ service/service.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Start(ctx context.Context, serviceName string, host, port string, registryHandlerFunc func()) (context.Context, error) {
+	registryHandlerFunc()
+	ctx = startService(ctx, serviceName, host, port)
+	return ctx, nil
+}
+
+func startService(ctx context.Context, serviceName string, host, port string) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+
+	var srv http.Server
+	srv.Addr = ":" + port
+
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+	go func() {
+		fmt.Printf("%v started. Press any key to stop.\n", serviceName)
+		var s string
+		_, _ = fmt.Scanln(&s)
+		_ = srv.Shutdown(ctx)
+		cancel()
+	}()
+	return ctx
+}
